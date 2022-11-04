39feef462cba5f59f9112997fa70a5bf121ed855
Supertiger <saawalkhn@googlemail.com>
2022-11-04 11:37:30 +0000

    chore: replaced text with i18n in FlowLogin

--- components/auth/src/flows/FlowLogin.tsx
+++ components/auth/src/flows/FlowLogin.tsx
@@ -3,11 +3,13 @@ import { Button } from "@revolt/ui";
 import { Fields, Form } from "./Form";
 import { useNavigate } from "@revolt/routing";
 import { clientController } from "../../../client";
+import { useTranslation } from "@revolt/i18n";
 
 /**
  * Flow for logging into an account
  */
 export default function FlowLogin() {
+  const t = useTranslation();
   const navigate = useNavigate();
 
   async function login(data: FormData) {
@@ -31,10 +33,10 @@ export default function FlowLogin() {
 
   return (
     <>
-      <FlowTitle subtitle="Sign into Revolt">Welcome back!</FlowTitle>
+      <FlowTitle subtitle={t('login.subtitle')}>{t('login.welcome')}</FlowTitle>
       <Form onSubmit={login} captcha={"3daae85e-09ab-4ff6-9f24-e8f4f335e433"}>
         <Fields fields={["email", "password"]} />
-        <Button type="submit">Login</Button>
+        <Button type="submit">{t('login.title')}</Button>
       </Form>
     </>
   );
