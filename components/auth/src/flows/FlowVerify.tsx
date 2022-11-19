49f64b66b53c8f119d37be32051ceb8970ab50cf
Paul Makles <me@insrt.uk>
2022-11-19 16:53:29 +0000

    feat(login): allow login after email verification

--- components/auth/src/flows/FlowVerify.tsx
+++ components/auth/src/flows/FlowVerify.tsx
@@ -1,19 +1,22 @@
 import { FlowTitle } from "./Flow";
 import { Button, Preloader, Typography } from "@revolt/ui";
-import { Link, useParams } from "@revolt/routing";
+import { Link, useNavigate, useParams } from "@revolt/routing";
 import { useTranslation } from "@revolt/i18n";
-import { createSignal, Match, onMount, Switch } from "solid-js";
+import { createSignal, Match, onMount, Show, Switch } from "solid-js";
 import { clientController, mapAnyError } from "@revolt/client";
 
-type State = {
-  state: 'verifying'
-} | {
-  state: 'error',
-  error: string
-} | {
-  state: 'success',
-  // ticket
-}
+type State =
+  | {
+      state: "verifying";
+    }
+  | {
+      state: "error";
+      error: string;
+    }
+  | {
+      state: "success";
+      mfa_ticket?: string;
+    };
 
 /**
  * Flow for confirming email
@@ -21,35 +24,59 @@ type State = {
 export default function FlowVerify() {
   const t = useTranslation();
   const params = useParams();
+  const navigate = useNavigate();
 
-  const [state, setState] = createSignal<State>({ state: 'success' });
-
-  onMount(() => {
-    /*clientController.getAnonymousClient().api.post(`/auth/account/verify/${params.token}`)
-      .then(() => setState({ state: 'success' }))
-      .catch(err => setState({ state: 'error', error: mapAnyError(err) }));*/
+  const [state, setState] = createSignal<State>({
+    state: "verifying",
   });
+
+  onMount(async () => {
+    try {
+      const data: any = await clientController
+        .getAnonymousClient()
+        .api.post(`/auth/account/verify/${params.token}`);
+
+      setState({ state: "success", mfa_ticket: data.ticket?.token });
+    } catch (err) {
+      setState({ state: "error", error: mapAnyError(err) });
+    }
+  });
+
+  async function login() {
+    const v = state();
+    if (v.state === "success" && v.mfa_ticket) {
+      await clientController.login({
+        mfa_ticket: v.mfa_ticket,
+      });
+
+      navigate("/", { replace: true });
+    }
+  }
 
   return (
     <Switch>
-      <Match when={state().state === 'verifying'}>
+      <Match when={state().state === "verifying"}>
         <FlowTitle>{t("login.verifying_account")}</FlowTitle>
         <Preloader type="ring" />
       </Match>
-      <Match when={state().state === 'error'}>
+      <Match when={state().state === "error"}>
         <FlowTitle>{t("login.error.verify")}</FlowTitle>
         <Typography variant="subtitle">
-          {t(`error.${(state() as State & { state: 'error' }).error}`, undefined, (state() as State & { state: 'error' }).error)}
+          {t(
+            `error.${(state() as State & { state: "error" }).error}`,
+            undefined,
+            (state() as State & { state: "error" }).error
+          )}
         </Typography>
         <Typography variant="subtitle">
           <Link href="../..">{t("login.remembered")}</Link>
         </Typography>
       </Match>
-      <Match when={state().state === 'success'}>
+      <Match when={state().state === "success"}>
         <FlowTitle>{t("login.verified_account")}</FlowTitle>
-        <Button>
-          {t("login.verified_continue")}
-        </Button>
+        <Show when={"mfa_ticket" in state()}>
+          <Button onClick={login}>{t("login.verified_continue")}</Button>
+        </Show>
         <Typography variant="subtitle">
           <Link href="../..">{t("login.remembered")}</Link>
         </Typography>
