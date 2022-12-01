846aad5743e16f78752b4b7b52a217e426c1222f
harshmangalam <harshdev8218@gmail.com>
2022-12-01 13:26:49 +0530

    feat(ui): port Tip component

---
+++ components/ui/components/design/atoms/display/Tip.tsx
@@ -0,0 +1,49 @@
+import { styled, css } from "solid-styled-components";
+import { AiOutlineInfoCircle } from "solid-icons/ai";
+import { JSX } from "solid-js";
+interface Props {
+  readonly palette?: "primary" | "success" | "warning" | "error";
+  readonly children: JSX.Element;
+}
+
+export const TipBase = styled.div<Omit<Props, "children">>`
+  display: flex;
+  gap: 10px;
+  padding: 12px;
+  font-size: 0.875rem;
+  font-weight: 500;
+  overflow: hidden;
+  background: var(--primary-header);
+  border-radius: var(--border-radius);
+  user-select: none;
+  a {
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  svg {
+    flex-shrink: 0;
+  }
+  ${(props) =>
+    props.palette && props.palette !== "primary"
+      ? css`
+          background: var(--${props.palette});
+          color: var(--${props.palette}-contrast);
+          border: 2px solid rgba(var(--${props.palette}-rgb), 0.8);
+        `
+      : css`
+          color: var(--foreground);
+          border: 2px solid var(--secondary-header);
+        `}
+`;
+
+export function Tip(props: Props) {
+  const { children, ...innerProps } = props;
+  return (
+    <TipBase {...innerProps}>
+      <AiOutlineInfoCircle size={20} />
+      {children}
+    </TipBase>
+  );
+}
