f3078f11332afb1108e2b54cd4cde9e4a63c961d
Paul Makles <me@insrt.uk>
2022-12-06 11:48:31 +0000

    feat: write more stories for Button

--- components/ui/components/design/atoms/inputs/Button.stories.tsx
+++ components/ui/components/design/atoms/inputs/Button.stories.tsx
@@ -1,5 +1,6 @@
-import type { ComponentStory } from '../../../stories';
 import { Button } from './Button'
+import { BiSolidBatteryLow, BiSolidUser } from 'solid-icons/bi';
+import type { ComponentStory } from '../../../stories';
 
 export default {
   category: 'Design System/Atoms/Inputs',
@@ -21,6 +22,51 @@ export default {
       title: 'Accent',
       props: {
         palette: 'accent'
+      }
+    },
+    {
+      title: 'Plain',
+      props: {
+        palette: 'plain'
+      }
+    },
+    {
+      title: 'Plain (Secondary)',
+      props: {
+        palette: 'plain-secondary'
+      }
+    },
+    {
+      title: 'Success',
+      props: {
+        palette: 'success'
+      }
+    },
+    {
+      title: 'Warning',
+      props: {
+        palette: 'warning'
+      }
+    },
+    {
+      title: 'Error',
+      props: {
+        palette: 'error'
+      }
+    },
+    {
+      title: 'Icon',
+      props: {
+        compact: 'icon',
+        children: <BiSolidUser size={16} />
+      }
+    },
+    {
+      title: 'Icon (Accent)',
+      props: {
+        compact: 'icon',
+        palette: 'accent',
+        children: <BiSolidBatteryLow size={16} />
       }
     }
   ],
