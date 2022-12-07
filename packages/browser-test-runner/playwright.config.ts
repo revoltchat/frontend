d278c913c4cd196fb51f3b8d459b7c92e6c62884
Paul Makles <me@insrt.uk>
2022-12-07 13:25:14 +0000

    chore(ci): skip Chromium tests in CI

--- packages/browser-test-runner/playwright.config.ts
+++ packages/browser-test-runner/playwright.config.ts
@@ -1,40 +1,50 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  testDir: './__tests__',
+  testDir: "./__tests__",
   timeout: 30 * 1000,
   expect: {
-    timeout: 5000
+    timeout: 5000,
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: "html",
   use: {
     actionTimeout: 0,
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
+  projects: process.env.CI
+    ? // Skip Chromium in CI for now because font rendering is inconsistent
+      [
+        {
+          name: "firefox",
+          use: {
+            ...devices["Desktop Firefox"],
+          },
+        },
+      ]
+    : [
+        {
+          name: "chromium",
+          use: {
+            ...devices["Desktop Chrome"],
+          },
+        },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-  ],
+        {
+          name: "firefox",
+          use: {
+            ...devices["Desktop Firefox"],
+          },
+        },
+      ],
 
-  outputDir: 'results/',
-  webServer: undefined
+  outputDir: "results/",
+  webServer: undefined,
 };
 
 export default config;
