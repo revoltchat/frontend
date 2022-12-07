ed0d90e0477210e8018e7c82495f24e089f0566b
Paul Makles <me@insrt.uk>
2022-12-07 13:24:18 +0000

    chore(showcase): auto select first story

--- packages/showcase/src/index.tsx
+++ packages/showcase/src/index.tsx
@@ -15,14 +15,13 @@ import {
   ScrollContainer,
   Tabs,
   Column,
-  Button,
   MenuButton,
   Typography,
 } from "@revolt/ui";
 import { SidebarBase } from "@revolt/ui/components/navigation/channels/common";
 import { Accessor, createSignal, For } from "solid-js";
 
-import components from './stories';
+import components from "./stories";
 
 const Container = styled(Column)`
   height: 100%;
@@ -42,8 +41,10 @@ const Content = styled(ScrollContainer)`
 `;
 
 render(() => {
-  const [component, select] = createSignal<keyof typeof components | undefined>();
-  const [tab, selectTab] = createSignal('');
+  const [component, select] = createSignal<
+    keyof typeof components | undefined
+  >();
+  const [tab, selectTab] = createSignal("");
   const [props, setProps] = createSignal<any>({});
 
   const tabs = () => {
@@ -55,34 +56,52 @@ render(() => {
       if (component.stories) {
         for (const story of component.stories) {
           obj[story.title] = {
-            label: story.title
+            label: story.title,
           };
         }
       }
     }
 
     return obj;
-  }
+  };
 
-  function render(component: string | undefined, tab: string, overwrittenProps: Accessor<any>) {
+  function render(
+    component: string | undefined,
+    tab: string,
+    overwrittenProps: Accessor<any>
+  ) {
     const entry = components[component!];
     if (entry) {
       const { component: Component, props, stories, effects } = entry;
 
-      const story = stories.find(story => story.title === tab);
+      const story = stories.find((story) => story.title === tab);
       if (story) {
         const effectProps: Record<string, (...args: any[]) => any> = {};
         if (effects) {
           for (const effect of Object.keys(effects)) {
-            effectProps[effect] = (...args) => setProps({...overwrittenProps(), ...(effects as any)[effect]({
-              ...props,
-              ...story.props,
-              ...overwrittenProps()
-            }, ...args)});
+            effectProps[effect] = (...args) =>
+              setProps({
+                ...overwrittenProps(),
+                ...(effects as any)[effect](
+                  {
+                    ...props,
+                    ...story.props,
+                    ...overwrittenProps(),
+                  },
+                  ...args
+                ),
+              });
           }
         }
 
-        return <Component {...props} {...story.props} {...overwrittenProps()} {...effectProps} />;
+        return (
+          <Component
+            {...props}
+            {...story.props}
+            {...overwrittenProps()}
+            {...effectProps}
+          />
+        );
       }
     }
 
@@ -94,28 +113,43 @@ render(() => {
       <Masks />
       <ThemeProvider theme={darkTheme}>
         <Container>
-        <Row gap="none" justify="stretch" grow>
-          <Sidebar>
-            <Column>
-              <Typography variant="h3">COMPONENTS</Typography>
-              <For each={Object.keys(components)}>
-                {entry => <Link onClick={() => {
+          <Row gap="none" justify="stretch" grow>
+            <Sidebar>
+              <Column>
+                <Typography variant="h3">COMPONENTS</Typography>
+                <For each={Object.keys(components)}>
+                  {(entry) => (
+                    <Link
+                      onClick={() => {
+                        setProps({});
+                        select(entry);
+                        selectTab(components[entry].stories[0]?.title);
+                      }}
+                    >
+                      <MenuButton
+                        attention={
+                          component() === entry ? "selected" : "normal"
+                        }
+                      >
+                        {entry}
+                      </MenuButton>
+                    </Link>
+                  )}
+                </For>
+              </Column>
+            </Sidebar>
+            <Column gap="none" grow>
+              <Tabs
+                tabs={tabs}
+                tab={tab}
+                onSelect={(tab) => {
                   setProps({});
-                  select(entry)
-                  }}><MenuButton attention={component() === entry ? 'selected' : 'normal'}>{entry}</MenuButton></Link>}
-              </For>
+                  selectTab(tab);
+                }}
+              />
+              <Content>{render(component(), tab(), props)}</Content>
             </Column>
-          </Sidebar>
-          <Column gap="none" grow>
-            <Tabs tabs={tabs} tab={tab} onSelect={tab => {
-              setProps({});
-              selectTab(tab);
-            }} />
-            <Content>
-              {render(component(), tab(), props)}
-            </Content>
-          </Column>
-        </Row>
+          </Row>
         </Container>
       </ThemeProvider>
     </div>
