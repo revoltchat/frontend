fa9856a25b3a3f9f72561cd61b97475182f9f337
Paul Makles <me@insrt.uk>
2022-12-07 18:53:37 +0000

    feat: write svg and text colour decorators

---
+++ components/ui/decorators/SvgDecorator.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from "solid-js";
+
+export default function makeSvgDecorator(viewSize: number, domSize: number) {
+    return ({ children }: { children: JSX.Element }) => <svg viewBox={`0 0 ${viewSize} ${viewSize}`} width={domSize} height={domSize}>{children}</svg>;
+}
---
+++ components/ui/decorators/TextColourDecorator.tsx
@@ -0,0 +1,5 @@
+import type { JSX } from "solid-js";
+
+export default function TextColourDecorator({ children }: { children: JSX.Element }) {
+    return <div style="color: white">{children}</div>
+}
