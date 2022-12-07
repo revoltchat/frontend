349e64f7956e62288b355b470cc4b8cca380e2d2
Paul Makles <me@insrt.uk>
2022-12-07 18:53:57 +0000

    chore: update stories types

--- components/ui/components/stories.d.ts
+++ components/ui/components/stories.d.ts
@@ -1,18 +1,23 @@
 import type { Component, ComponentProps } from "solid-js";
+import type { JSX } from "solid-js";
+
+type Decorator = Component<{ children: JSX.Element }>;
 
 declare type Story<T extends Component<P>, P = {}> = {
   title: string;
   props?: ComponentProps<T>;
+  decorators?: Decorator[];
 };
 
-type ComponentStory<T extends Component<P>, P = {}> = {
+declare type ComponentStory<T extends Component<P>, P = {}> = {
   category?: string;
   component: T;
   stories: Story<T, P>[];
   props?: ComponentProps<T>;
+  decorators?: Decorator[];
   propTypes?: Record<
     keyof ComponentProps<T>,
-    "string" | "boolean" | "function" | (string | boolean)[]
+    "string" | "boolean" | "number" | "function" | "component" | (string | boolean)[]
   >;
   effects?: Record<
     keyof ComponentProps<T>,
