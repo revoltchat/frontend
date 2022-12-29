8ddb98925a68b2c1df1ea98dfdac09f7b466036e
Paul Makles <me@insrt.uk>
2022-12-29 15:30:50 +0000

    fix(ui): convert nullable date to timestamp no.

--- components/ui/components/messaging/message/Message.tsx
+++ components/ui/components/messaging/message/Message.tsx
@@ -7,17 +7,14 @@ import { Embed } from "./Embed";
 /**
  * Render a Message with or without a tail
  */
-export function Message(props: {
-  message: MessageInterface;
-  tail?: boolean;
-}) {
+export function Message(props: { message: MessageInterface; tail?: boolean }) {
   return (
     <MessageContainer
       username={props.message.username}
       avatar={props.message.avatarURL}
       colour={props.message.roleColour}
       timestamp={props.message.createdAt}
-      edited={props.message.edited}
+      edited={props.message.edited ? +props.message.edited : undefined}
       tail={props.tail}
       header={
         <Show when={props.message.reply_ids}>
@@ -30,16 +27,19 @@ export function Message(props: {
       {props.message.content}
       <Show when={props.message.attachments}>
         <For each={props.message.attachments}>
-          {attachment => <Attachment file={attachment} baseUrl={props.message.client.configuration?.features.autumn.url!} />}
+          {(attachment) => (
+            <Attachment
+              file={attachment}
+              baseUrl={props.message.client.configuration?.features.autumn.url!}
+            />
+          )}
         </For>
       </Show>
       <Show when={props.message.embeds}>
         <For each={props.message.embeds}>
-          {embed=>
-          <Embed
-            embed={embed}
-            proxyFile={props.message.client.proxyFile}
-          />}
+          {(embed) => (
+            <Embed embed={embed} proxyFile={props.message.client.proxyFile} />
+          )}
         </For>
       </Show>
     </MessageContainer>
