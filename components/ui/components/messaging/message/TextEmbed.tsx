a994a474012cb72de7bbc56c14caa8f5d518e7a7
Paul Makles <me@insrt.uk>
2022-12-29 15:14:11 +0000

    feat(ui): render text embeds

---
+++ components/ui/components/messaging/message/TextEmbed.tsx
@@ -0,0 +1,159 @@
+import type { Embed, E } from "./Embed";
+import { ComponentProps, Match, Show, Switch } from "solid-js";
+import { styled } from "solid-styled-components";
+import {
+  Column,
+  OverflowingText,
+  SizedContent,
+  Typography,
+} from "../../design";
+import { Attachment } from "./Attachment";
+import { SpecialEmbed } from "./SpecialEmbed";
+
+const Base = styled("div", "TextEmbed")<{ borderColour?: string }>`
+  display: flex;
+  flex-direction: row;
+  gap: ${(props) => props.theme!.gap.md};
+
+  padding: ${(props) => props.theme!.gap.md};
+  color: ${(props) => props.theme!.colours["foreground"]};
+  border-radius: ${(props) => props.theme!.borderRadius.md};
+  background: ${(props) => props.theme!.colours["background-400"]};
+
+  border-inline-start: 4px solid
+    ${(props) => props.borderColour ?? props.theme!.colours["background-200"]};
+`;
+
+const SiteInformation = styled("div", "SiteInfo")`
+  display: flex;
+  flex-direction: row;
+  gap: ${(props) => props.theme!.gap.md};
+  color: ${(props) => props.theme!.colours["foreground-100"]};
+`;
+
+const Favicon = styled("img", "Favicon")`
+  width: 14px;
+  height: 14px;
+  flex-shrink: 0;
+`;
+
+const PreviewImage = styled("img", "PreviewImage")`
+  max-width: 120px;
+  max-height: 120px;
+  border-radius: ${(props) => props.theme!.borderRadius.md};
+`;
+
+// TODO: move all font sizes into typography
+const Title = styled("div", "Title")`
+  font-size: 16px;
+`;
+
+const Description = styled("div", "Description")`
+  font-size: 12px;
+`;
+
+export function TextEmbed(
+  props: ComponentProps<typeof Embed> & {
+    embed: { type: "Text" | "Website" };
+  }
+) {
+  return (
+    <Base borderColour={props.embed.colour!}>
+      <Column gap="md">
+        <Show when={props.embed.type === "Website" && props.embed.site_name}>
+          <SiteInformation>
+            <Show when={props.embed.icon_url}>
+              <Favicon
+                loading="lazy"
+                draggable={false}
+                src={props.proxyFile(props.embed.icon_url!)}
+                onError={(e) => (e.currentTarget.style.display = "none")}
+              />
+            </Show>
+            <Typography variant="small">
+              <OverflowingText>
+                {(props.embed as E<"Website">).site_name}
+              </OverflowingText>
+            </Typography>
+          </SiteInformation>
+        </Show>
+
+        <Show when={props.embed.title}>
+          <Title>
+            <a>{props.embed.title}</a>
+          </Title>
+        </Show>
+
+        <Show when={props.embed.description}>
+          <Description>
+            <Switch fallback={props.embed.description}>
+              <Match when={props.embed.type === "Text"}>
+                {props.embed.description /** TODO: render markdown */}
+              </Match>
+            </Switch>
+          </Description>
+        </Show>
+
+        <Show when={props.embed.type === "Text" && props.embed.media}>
+          <Attachment
+            baseUrl="http://local.revolt.chat:5273"
+            file={(props.embed as E<"Text">).media!}
+          />
+        </Show>
+
+        <Show when={props.embed.type === "Website"}>
+          <Switch>
+            <Match
+              when={
+                (props.embed as E<"Website">).special?.type &&
+                (props.embed as E<"Website">).special?.type !== "None"
+              }
+            >
+              <SpecialEmbed embed={props.embed as E<"Website">} />
+            </Match>
+            <Match when={(props.embed as E<"Website">).video}>
+              <SizedContent
+                width={(props.embed as E<"Website">).video!.width}
+                height={(props.embed as E<"Website">).video!.height}
+              >
+                <video
+                  controls
+                  preload="metadata"
+                  src={props.proxyFile(
+                    (props.embed as E<"Website">).video!.url
+                  )}
+                />
+              </SizedContent>
+            </Match>
+            <Match when={(props.embed as E<"Website">).image?.size === "Large"}>
+              <SizedContent
+                width={(props.embed as E<"Website">).image!.width}
+                height={(props.embed as E<"Website">).image!.height}
+              >
+                <img
+                  src={props.proxyFile(
+                    (props.embed as E<"Website">).image!.url
+                  )}
+                  loading="lazy"
+                />
+              </SizedContent>
+            </Match>
+          </Switch>
+        </Show>
+      </Column>
+
+      <Show
+        when={
+          props.embed.type === "Website" &&
+          props.embed.image?.size === "Preview" &&
+          !props.embed.video
+        }
+      >
+        <PreviewImage
+          src={props.proxyFile((props.embed as E<"Website">).image!.url)}
+          loading="lazy"
+        />
+      </Show>
+    </Base>
+  );
+}
