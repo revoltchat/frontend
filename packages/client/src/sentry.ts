e5f309343f77698e120ac0447148133f903d4e0b
Paul Makles <me@insrt.uk>
2023-03-05 17:39:50 +0000

    chore: only run sentry in prod

--- packages/client/src/sentry.ts
+++ packages/client/src/sentry.ts
@@ -1,8 +1,10 @@
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: "https://d03b3660658f45e4be733282d2d5fd9d@sentry.insert.moe/9",
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: "https://d03b3660658f45e4be733282d2d5fd9d@sentry.insert.moe/9",
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
