9de092db6e1927829fc7baa338991e7761d722a1
StationaryStation <raulytstation@outlook.com>
2023-04-19 18:11:17 -0400

    feat(ui): start working on radio inputs

--- components/ui/components/design/atoms/inputs/Radio.tsx
+++ components/ui/components/design/atoms/inputs/Radio.tsx
@@ -1,5 +1,5 @@
 import { FaSolidCircle as CircleIcon } from "solid-icons/fa";
-import { JSX, Show } from "solid-js";
+import { JSX, Show, createSignal, splitProps } from "solid-js";
 import { styled } from "solid-styled-components";
 
 interface Props {
@@ -7,7 +7,7 @@ interface Props {
   description?: JSX.Element;
   disabled?: boolean;
   value?: boolean;
-  onSelect?: () => void;
+  onSelect?: (state: boolean) => void;
 }
 
 interface BaseProps {
@@ -15,14 +15,14 @@ interface BaseProps {
 }
 
 const Base = styled.label<BaseProps>`
-  border: 2px solid var(--tertiary-foreground);
+  border: 2px solid ${(props) => props.theme?.colours["background-400"]};
   padding: 10px;
   gap: 10px;
   display: flex;
   cursor: pointer;
   user-select: none;
-  transition: 0.1s ease-in-out all;
-  border-radius: var(--border-radius);
+  transition: ${(props) => props.theme?.transitions.fast};
+  border-radius: ${(props) => props.theme?.borderRadius.md};
 
   input {
     display: none;
@@ -31,16 +31,16 @@ const Base = styled.label<BaseProps>`
   ${(props) =>
     props.selected
       ? `
-          color: var(--accent-contrast);
+          color: ${props.theme?.colours["background-100"]};
           cursor: default;
-          background: var(--accent);
-          border: 2px solid var(--accent);
+          background: ${props.theme?.colours.accent};
+          border: 2px solid ${props.theme?.colours.accent};
 
           div {
-            border-color: var(--accent-contrast);
+            border-color:  ${props.theme?.colours["background-100"]};
 
             svg {
-              color: var(--accent-contrast);
+              color:  ${props.theme?.colours["background-100"]};
               visibility: visible;
               opacity: 1;
             }
@@ -52,7 +52,7 @@ const Base = styled.label<BaseProps>`
     !props.selected
       ? `
           &:hover {
-            background: var(--hover);
+            background: ${props.theme?.colours["background-100"]};
 
             svg {
               visibility: visible;
@@ -68,15 +68,15 @@ const RadioCircle = styled.div`
   align-items: center;
   justify-content: center;
   flex-shrink: 0;
-  border: 2px solid var(--tertiary-foreground);
-  border-radius: var(--border-radius-half);
+  border: 2px solid ${(props) => props.theme?.colours["background-400"]};
+  border-radius: ${(props) => props.theme?.borderRadius.lg};
   height: 20px;
   width: 20px;
   transition: inherit;
 
   svg {
     transition: inherit;
-    color: var(--tertiary-foreground);
+    color: ${(props) => props.theme?.colours["background-400"]};
     flex-shrink: 0;
     visibility: hidden;
     opacity: 0;
@@ -93,7 +93,7 @@ const Content = styled.div`
 const Title = styled.div<BaseProps>`
   font-size: 0.875rem;
   font-weight: 600;
-  color: var(--foreground);
+  color: ${(props) => props.theme?.colours.foreground};
   transition: inherit;
 
   display: -webkit-box;
@@ -104,7 +104,7 @@ const Title = styled.div<BaseProps>`
   ${(props) =>
     props.selected
       ? `
-          color: var(--accent-contrast);
+          color: ${props.theme?.colours["background-100"]};
         `
       : undefined}
 `;
@@ -112,7 +112,7 @@ const Title = styled.div<BaseProps>`
 const Description = styled.div<BaseProps>`
   font-size: 0.6875rem;
   font-weight: 500;
-  color: var(--secondary-foreground);
+  color: ${(props) => props.theme?.colours["foreground-200"]};
   transition: inherit;
 
   display: -webkit-box;
@@ -129,27 +129,37 @@ const Description = styled.div<BaseProps>`
 `;
 
 export function Radio(props: Props) {
-  const selected = props.value ?? false;
+  const [local, others] = splitProps(props, [
+    "disabled",
+    "title",
+    "description",
+    "value",
+    "onSelect",
+  ]);
+
+  const [controlledValue, setControlledValue] = createSignal(false);
+  const selected = () => local.value ?? controlledValue();
+
   return (
-    <Base selected={selected}>
+    <Base {...others} selected={selected()}>
       <RadioCircle>
         <CircleIcon size={12} />
       </RadioCircle>
       <input
         type="radio"
-        checked={selected}
-        onChange={(e) =>
-          e.currentTarget.value === "on" &&
-          !props.disabled &&
-          props.onSelect?.()
+        checked={selected()}
+        onChange={() =>
+          typeof local.value !== "undefined"
+            ? !local.disabled && local.onSelect?.(!local.value)
+            : setControlledValue((v) => !v)
         }
       />
       <Content>
         <Show when={props.title}>
-          <Title selected={selected}>{props.title}</Title>
+          <Title selected={selected()}>{props.title}</Title>
         </Show>
         <Show when={props.description}>
-          <Description selected={selected}>{props.description}</Description>
+          <Description selected={selected()}>{props.description}</Description>
         </Show>
       </Content>
     </Base>
