01edec86c8386592802feb9d4e602fde6d21e138
Paul Makles <me@insrt.uk>
2023-04-20 18:57:45 +0100

    chore: clean up debug code

--- packages/client/src/Interface.tsx
+++ packages/client/src/Interface.tsx
@@ -1,7 +1,8 @@
-import { Component, Match, Switch, createSignal } from "solid-js";
+import { Component, Match, Switch } from "solid-js";
 
 import { clientController } from "@revolt/client";
 import { Navigate } from "@revolt/routing";
+import { Preloader } from "@revolt/ui";
 
 import { Content } from "./interface/Content";
 import { Sidebar } from "./interface/Sidebar";
@@ -10,35 +11,12 @@ import { Sidebar } from "./interface/Sidebar";
  * Application layout
  */
 const Interface: Component = () => {
-  /**
-   * Check whether we are ready to display the interface
-   * @returns Boolean
-   */
-  const ready = () => clientController.isReady();
-
-  const [yes, doIt] = createSignal(false);
-
   return (
-    <Switch
-      fallback={
-        <h1>
-          we appear to be loading; ready:{" "}
-          {clientController.isReady() ? "yes" : "no"}; ready:{" "}
-          {ready() ? "yes" : "no"}; logged in:{" "}
-          {clientController.isLoggedIn() ? "yes" : "no"}; do we have a client?{" "}
-          {"" + clientController.getCurrentClient()}; direct call(
-          {"" + clientController.getCurrentClient()?.ready}) ={" "}
-          {"" + clientController.getCurrentClient()?.ready()}
-          <br />
-          <button onClick={() => doIt(true)}>open sesame</button>
-        </h1>
-      }
-    >
+    <Switch fallback={<Preloader grow type="spinner" />}>
       <Match when={!clientController.isLoggedIn()}>
-        <h1>Redirecting...</h1>
         <Navigate href="/login" />
       </Match>
-      <Match when={yes() || ready()}>
+      <Match when={clientController.isReady()}>
         <div
           style={{
             display: "flex",
