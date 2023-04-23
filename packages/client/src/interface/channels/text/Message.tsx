67963a9e057a7ae81a0dc3195fb21b94a2e37e1c
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2023-04-24 00:44:31 +0700

    feat: including "replying to" in copy text (#172)

--- packages/client/src/interface/channels/text/Message.tsx
+++ packages/client/src/interface/channels/text/Message.tsx
@@ -28,6 +28,7 @@ import {
   styled,
 } from "@revolt/ui";
 import { MessageReply } from "@revolt/ui/components/messaging/message/MessageReply";
+import { MessageReplyCopyHelper } from "@revolt/ui/components/messaging/message/MessageReplyCopyHelper";
 
 /**
  * Regex for matching URLs
@@ -78,150 +79,185 @@ export function Message(props: Props) {
   const unreact = (emoji: string) => props.message.unreact(emoji);
 
   return (
-    <MessageContainer
-      username={
-        <UserCard user={props.message.author!} member={props.message.member}>
-          {(triggerProps) => (
-            <Username
-              {...triggerProps}
-              username={props.message.username}
-              colour={props.message.roleColour!}
-            />
-          )}
-        </UserCard>
-      }
-      avatar={props.message.avatarURL}
-      timestamp={props.message.createdAt}
-      edited={props.message.editedAt}
-      tail={props.tail}
-      header={
-        <Show when={props.message.replyIds}>
-          <For each={props.message.replyIds}>
-            {(reply_id) => {
-              /**
-               * Signal the actual message
-               */
-              const message = () => client().messages.get(reply_id);
+    <>
+      <Show when={props.message.replyIds}>
+        {/* {props.message.replyIds} */}
+        <For each={props.message.replyIds}>
+          {(reply_id) => {
+            /**
+             * Signal the actual message
+             */
+            const message = () => client().messages.get(reply_id);
 
-              onMount(() => {
-                if (!message()) {
-                  props.message.channel!.fetchMessage(reply_id);
-                }
-              });
+            onMount(() => {
+              if (!message()) {
+                props.message.channel!.fetchMessage(reply_id);
+              }
+            });
 
-              return (
-                <MessageReply
-                  mention={props.message.mentionIds?.includes(
-                    message()!.authorId!
-                  )}
-                  message={message()}
-                />
-              );
-            }}
-          </For>
-        </Show>
-      }
-      info={
-        <Switch fallback={<div />}>
-          <Match
-            when={
-              props.message.authorId &&
-              dayjs().diff(decodeTime(props.message.authorId), "day") < 1
-            }
-          >
-            <NewUser>
-              <Tooltip content={"New to Revolt"} placement="top" aria>
-                {(triggerProps) => <BiSolidLeaf {...triggerProps} size={16} />}
-              </Tooltip>
-            </NewUser>
-          </Match>
-          <Match
-            when={
-              props.message.member &&
-              dayjs().diff(props.message.member.joinedAt, "day") < 1
-            }
-          >
-            <NewUser>
-              <Tooltip content={"New to the server"} placement="top" aria>
-                {(triggerProps) => <BiSolidLeaf {...triggerProps} size={16} />}
-              </Tooltip>
-            </NewUser>
-          </Match>
-          <Match
-            when={
-              props.message.masquerade &&
-              props.message.authorId === "01FHGJ3NPP7XANQQH8C2BE44ZY"
-            }
-          >
-            <Tooltip
-              content={t("app.main.channel.bridged")}
-              placement="top"
-              aria
+            return <MessageReplyCopyHelper message={message()!} />;
+          }}
+        </For>
+      </Show>
+      <MessageContainer
+        username={
+          <UserCard user={props.message.author!} member={props.message.member}>
+            {(triggerProps) => (
+              <Username
+                {...triggerProps}
+                username={props.message.username}
+                colour={props.message.roleColour!}
+              />
+            )}
+          </UserCard>
+        }
+        avatar={props.message.avatarURL}
+        timestamp={props.message.createdAt}
+        edited={props.message.editedAt}
+        tail={props.tail}
+        header={
+          <Show when={props.message.replyIds}>
+            <For each={props.message.replyIds}>
+              {(reply_id) => {
+                /**
+                 * Signal the actual message
+                 */
+                const message = () => client().messages.get(reply_id);
+
+                onMount(() => {
+                  if (!message()) {
+                    props.message.channel!.fetchMessage(reply_id);
+                  }
+                });
+
+                return (
+                  <MessageReply
+                    mention={props.message.mentionIds?.includes(
+                      message()!.authorId!
+                    )}
+                    message={message()}
+                  />
+                );
+              }}
+            </For>
+          </Show>
+        }
+        info={
+          <Switch fallback={<div />}>
+            <Match
+              when={
+                props.message.authorId &&
+                dayjs().diff(decodeTime(props.message.authorId), "day") < 1
+              }
             >
-              {(triggerProps) => <BiRegularLink {...triggerProps} size={16} />}
-            </Tooltip>
+              <NewUser>
+                <Tooltip content={"New to Revolt"} placement="top" aria>
+                  {(triggerProps) => (
+                    <BiSolidLeaf {...triggerProps} size={16} />
+                  )}
+                </Tooltip>
+              </NewUser>
+            </Match>
+            <Match
+              when={
+                props.message.member &&
+                dayjs().diff(props.message.member.joinedAt, "day") < 1
+              }
+            >
+              <NewUser>
+                <Tooltip content={"New to the server"} placement="top" aria>
+                  {(triggerProps) => (
+                    <BiSolidLeaf {...triggerProps} size={16} />
+                  )}
+                </Tooltip>
+              </NewUser>
+            </Match>
+            <Match
+              when={
+                props.message.masquerade &&
+                props.message.authorId === "01FHGJ3NPP7XANQQH8C2BE44ZY"
+              }
+            >
+              <Tooltip
+                content={t("app.main.channel.bridged")}
+                placement="top"
+                aria
+              >
+                {(triggerProps) => (
+                  <BiRegularLink {...triggerProps} size={16} />
+                )}
+              </Tooltip>
+            </Match>
+            <Match when={props.message.author?.privileged}>
+              <Tooltip
+                content={t("app.main.channel.team")}
+                placement="top"
+                aria
+              >
+                {(triggerProps) => (
+                  <BiSolidShield {...triggerProps} size={16} />
+                )}
+              </Tooltip>
+            </Match>
+            <Match when={props.message.author?.bot}>
+              <Tooltip content={t("app.main.channel.bot")} placement="top" aria>
+                {(triggerProps) => <BiSolidBot {...triggerProps} size={16} />}
+              </Tooltip>
+            </Match>
+            <Match
+              when={props.message.authorId === "01EX2NCWQ0CHS3QJF0FEQS1GR4"}
+            >
+              <span />
+              <span>he/him &middot; </span>
+            </Match>
+          </Switch>
+        }
+        compact={
+          !!props.message.systemMessage ||
+          state.settings.getValue("appearance:compact_mode")
+        }
+        infoMatch={
+          <Match when={props.message.systemMessage}>
+            <SystemMessageIcon
+              systemMessage={props.message.systemMessage!}
+              createdAt={props.message.createdAt}
+              isServer={!!props.message.server}
+            />
           </Match>
-          <Match when={props.message.author?.privileged}>
-            <Tooltip content={t("app.main.channel.team")} placement="top" aria>
-              {(triggerProps) => <BiSolidShield {...triggerProps} size={16} />}
-            </Tooltip>
-          </Match>
-          <Match when={props.message.author?.bot}>
-            <Tooltip content={t("app.main.channel.bot")} placement="top" aria>
-              {(triggerProps) => <BiSolidBot {...triggerProps} size={16} />}
-            </Tooltip>
-          </Match>
-          <Match when={props.message.authorId === "01EX2NCWQ0CHS3QJF0FEQS1GR4"}>
-            <span />
-            <span>he/him &middot; </span>
-          </Match>
-        </Switch>
-      }
-      compact={
-        !!props.message.systemMessage ||
-        state.settings.getValue("appearance:compact_mode")
-      }
-      infoMatch={
-        <Match when={props.message.systemMessage}>
-          <SystemMessageIcon
-            systemMessage={props.message.systemMessage!}
-            createdAt={props.message.createdAt}
-            isServer={!!props.message.server}
+        }
+      >
+        <Column gap="sm">
+          <Show when={props.message.systemMessage}>
+            <SystemMessage
+              systemMessage={props.message.systemMessage!}
+              isServer={!!props.message.server}
+            />
+          </Show>
+          <Show when={props.message.content && !isOnlyGIF()}>
+            <BreakText>
+              <Markdown content={props.message.content!} />
+            </BreakText>
+          </Show>
+          <Show when={props.message.attachments}>
+            <For each={props.message.attachments}>
+              {(attachment) => <Attachment file={attachment} />}
+            </For>
+          </Show>
+          <Show when={props.message.embeds}>
+            <For each={props.message.embeds}>
+              {(embed) => <Embed embed={embed} />}
+            </For>
+          </Show>
+          <Reactions
+            reactions={props.message.reactions}
+            interactions={props.message.interactions}
+            userId={client().user!.id}
+            addReaction={react}
+            removeReaction={unreact}
           />
-        </Match>
-      }
-    >
-      <Column gap="sm">
-        <Show when={props.message.systemMessage}>
-          <SystemMessage
-            systemMessage={props.message.systemMessage!}
-            isServer={!!props.message.server}
-          />
-        </Show>
-        <Show when={props.message.content && !isOnlyGIF()}>
-          <BreakText>
-            <Markdown content={props.message.content!} />
-          </BreakText>
-        </Show>
-        <Show when={props.message.attachments}>
-          <For each={props.message.attachments}>
-            {(attachment) => <Attachment file={attachment} />}
-          </For>
-        </Show>
-        <Show when={props.message.embeds}>
-          <For each={props.message.embeds}>
-            {(embed) => <Embed embed={embed} />}
-          </For>
-        </Show>
-        <Reactions
-          reactions={props.message.reactions}
-          interactions={props.message.interactions}
-          userId={client().user!.id}
-          addReaction={react}
-          removeReaction={unreact}
-        />
-      </Column>
-    </MessageContainer>
+        </Column>
+      </MessageContainer>
+    </>
   );
 }
 
