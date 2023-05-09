afd6e67c4755270b2a6b4bace5f02d973d5b7bfa
Isa <nfleishman@student.ccaeagles.org>
2023-05-09 17:02:26 -0400

    Update UserCard.tsx
    a little update to the user card

--- components/ui/components/floating/UserCard.tsx
+++ components/ui/components/floating/UserCard.tsx
@@ -17,8 +17,31 @@ const Base = styled("div", "Tooltip")`
   background: black;
   width: 400px;
   height: 400px;
+  background: ${({ theme }) => theme!.colours["background-300"]};
 `;
 
+const profiletopcontainerStyle = {
+    position: 'relative',
+    width: '100%',
+    height: '350px',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    marginBottom: '20px'
+};
+
+const profileimageStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-200%, -150%)',
+    "border-radius": '50%',
+    width: '85px',
+    height: '85px',
+    "object-fit": 'cover',
+    overflow: 'hidden',
+    zIndex: '1'
+};
+//{'border-radius': '50%', width: '85px', height: '85px', "object-fit": "cover", overflow: "hidden"}
 interface Props {
   /**
    * User card trigger area
@@ -81,11 +104,17 @@ export function UserCard(props: Props) {
                   position: position.strategy,
                   top: `${position.y ?? 0}px`,
                   left: `${position.x ?? 0}px`,
+                  'border-radius': '2%',
+                  padding: '1%',
                 }}
-                role="tooltip"
-              >
-                {props.user.username}
-                <br />
+                role="tooltip">
+                <div style={profiletopcontainerStyle}>
+                  <img src={props.user.animatedAvatarURL} style={{width: '100%', height: '30%'}} ></img>
+                  <h2 style={{ 'text-align': 'center' }}>{props.user.username}</h2>
+                  <img src={props.user.animatedAvatarURL} style={profileimageStyle}></img>
+                  ID: {props.user.id}<br/>Badges: {props.user.badges}<br/>Status: {props.user.status?.presence}<br/> Nickname: {props.member?.nickname} <br/>Flags?: {props.user.flags} <br/> Privalledge???: {props.user.privileged} <br/> created at: {props.user.fetchProfile()} <br/> {props.user.relationship} <br/> {props.user.permission}
+                  <br />
+                </div>
                 <Show when={props.member}>
                   <For each={props.member!.orderedRoles}>
                     {(role) => (
