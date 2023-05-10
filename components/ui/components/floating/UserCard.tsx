b02e569002483f8bada7f07cd7e0bafdf584db29
Isa <nfleishman@student.ccaeagles.org>
2023-05-10 18:13:57 +0000

    more user card

--- components/ui/components/floating/UserCard.tsx
+++ components/ui/components/floating/UserCard.tsx
@@ -1,7 +1,7 @@
 import { useFloating } from "solid-floating-ui";
 import { For, JSX, Ref, Show, createSignal } from "solid-js";
 import { Portal } from "solid-js/web";
-import { styled } from "solid-styled-components";
+import { ThemeProvider, styled } from "solid-styled-components";
 
 import { autoUpdate, flip, offset, shift } from "@floating-ui/dom";
 import { Motion, Presence } from "@motionone/solid";
@@ -20,27 +20,55 @@ const Base = styled("div", "Tooltip")`
   background: ${({ theme }) => theme!.colours["background-300"]};
 `;
 
+const ProfilePictureContainer = styled.div`
+  position: relative;
+  width: 85px;
+  height: 85px;
+`;
+
 const profiletopcontainerStyle = {
-    position: 'relative',
-    width: '100%',
-    height: '350px',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    marginBottom: '20px'
+  position: 'relative',
+  width: '100%',
+  height: '350px',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  marginBottom: '20px'
 };
 
+
+
 const profileimageStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-200%, -150%)',
-    "border-radius": '50%',
-    width: '85px',
-    height: '85px',
-    "object-fit": 'cover',
-    overflow: 'hidden',
-    zIndex: '1'
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-220%, -150%)',
+  "border-radius": '50%',
+  width: '85px',
+  height: '85px',
+  "object-fit": 'cover',
+  overflow: 'hidden',
+  zIndex: '1',
 };
+
+const StatusIndicator = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: white
+`;
+
+const GrayOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  content: "";
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 //{'border-radius': '50%', width: '85px', height: '85px', "object-fit": "cover", overflow: "hidden"}
 interface Props {
   /**
@@ -104,17 +132,26 @@ export function UserCard(props: Props) {
                   position: position.strategy,
                   top: `${position.y ?? 0}px`,
                   left: `${position.x ?? 0}px`,
-                  'border-radius': '2%',
-                  padding: '1%',
+                  'border-radius': '3%',
+                  overflow: "hidden"
                 }}
                 role="tooltip">
                 <div style={profiletopcontainerStyle}>
-                  <img src={props.user.animatedAvatarURL} style={{width: '100%', height: '30%'}} ></img>
-                  <h2 style={{ 'text-align': 'center' }}>{props.user.username}</h2>
+                  <div style={{width: '100%', height: '30%', overflow: "hidden"}}>
+                    <img src={props.user.animatedAvatarURL} style={{ width: '100%', height: '100%'}} ></img>
+                    <GrayOverlay style={{width:'100%', height: '30%'}} />
+                  </div>
+                  
+                  
                   <img src={props.user.animatedAvatarURL} style={profileimageStyle}></img>
-                  ID: {props.user.id}<br/>Badges: {props.user.badges}<br/>Status: {props.user.status?.presence}<br/> Nickname: {props.member?.nickname} <br/>Flags?: {props.user.flags} <br/> Privalledge???: {props.user.privileged} <br/> created at: {props.user.fetchProfile()} <br/> {props.user.relationship} <br/> {props.user.permission}
+                  <StatusIndicator style={{"background-color": "#ffffff", transform: 'translate(-1540%, -1100%)'}}/>
+                  <h2 style={{transform: 'translate(3.5%, 30%)'}}>@{props.user.username}</h2>
+                  {props.member?.nickname ? <p style={{"padding-left": "9%", "padding-top": "0.5%", margin: "0px", transform: 'translate(0%, -60%)'}}>AKA: {props.member?.nickname}</p> : null}
+            
+                  ID: {props.user.id}<br />Badges: {props.user.badges}<br />Status: {props.user.status?.presence}<br />Flags?: {props.user.flags} <br /> Privalledge???: {props.user.privileged} <br /> created at: {props.user.fetchProfile()} <br /> {props.user.relationship} <br /> {props.user.permission}
                   <br />
                 </div>
+
                 <Show when={props.member}>
                   <For each={props.member!.orderedRoles}>
                     {(role) => (
