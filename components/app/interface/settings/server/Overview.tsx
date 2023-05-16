0d9a88d6335e518566b11961a9c90034ab5bb386
Paul Makles <me@insrt.uk>
2023-05-16 18:19:49 +0100

    feat: allow switching server flags from overview

--- components/app/interface/settings/server/Overview.tsx
+++ components/app/interface/settings/server/Overview.tsx
@@ -1,8 +1,17 @@
-import { BiRegularAbacus, BiRegularGlobe } from "solid-icons/bi";
+import { BiRegularAbacus, BiRegularGlobe, BiSolidFlag } from "solid-icons/bi";
 import { Show } from "solid-js";
 
+import { ServerFlags } from "revolt.js";
+
 import { useClient } from "@revolt/client";
-import { CategoryButton, Checkbox, Column, Typography } from "@revolt/ui";
+import {
+  CategoryButton,
+  CategoryCollapse,
+  Checkbox,
+  Column,
+  FormGroup,
+  Typography,
+} from "@revolt/ui";
 
 import { ServerSettingsProps } from ".";
 
@@ -17,7 +26,67 @@ export default function ServerOverview(props: ServerSettingsProps) {
       <Show when={user().user?.privileged}>
         <Column>
           <Typography variant="label">Platform</Typography>
-          <label>
+          <CategoryCollapse
+            title="Flags"
+            icon={<BiSolidFlag size={24} />}
+            description="Set visible badges on server"
+          >
+            <FormGroup>
+              <CategoryButton
+                icon="blank"
+                action={
+                  <Checkbox
+                    value={!props.server.flags}
+                    onChange={() =>
+                      props.server.edit({
+                        flags: 0,
+                      })
+                    }
+                  />
+                }
+                onClick={() => void 0}
+              >
+                No flags
+              </CategoryButton>
+            </FormGroup>
+            <FormGroup>
+              <CategoryButton
+                icon="blank"
+                action={
+                  <Checkbox
+                    value={props.server.flags === ServerFlags.Official}
+                    onChange={() =>
+                      props.server.edit({
+                        flags: ServerFlags.Official,
+                      })
+                    }
+                  />
+                }
+                onClick={() => void 0}
+              >
+                Official Server
+              </CategoryButton>
+            </FormGroup>
+            <FormGroup>
+              <CategoryButton
+                icon="blank"
+                action={
+                  <Checkbox
+                    value={props.server.flags === ServerFlags.Verified}
+                    onChange={() =>
+                      props.server.edit({
+                        flags: ServerFlags.Verified,
+                      })
+                    }
+                  />
+                }
+                onClick={() => void 0}
+              >
+                Verified Server
+              </CategoryButton>
+            </FormGroup>
+          </CategoryCollapse>
+          <FormGroup>
             <CategoryButton
               description="Message counts will be collected for server"
               icon={<BiRegularAbacus size={24} />}
@@ -35,8 +104,8 @@ export default function ServerOverview(props: ServerSettingsProps) {
             >
               Analytics
             </CategoryButton>
-          </label>
-          <label>
+          </FormGroup>
+          <FormGroup>
             <CategoryButton
               description="Server can be joined from Discover"
               icon={<BiRegularGlobe size={24} />}
@@ -54,7 +123,7 @@ export default function ServerOverview(props: ServerSettingsProps) {
             >
               Public Server
             </CategoryButton>
-          </label>
+          </FormGroup>
         </Column>
       </Show>
     </Column>
