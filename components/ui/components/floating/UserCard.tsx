bc09e72592844cca9ca9eb1100ee39a5230066ba
Paul Makles <me@insrt.uk>
2023-05-20 15:45:49 +0100

    feat: add roles to members from user card

--- components/ui/components/floating/UserCard.tsx
+++ components/ui/components/floating/UserCard.tsx
@@ -1,8 +1,8 @@
-import type { JSX } from "solid-js";
+import { JSX, createMemo } from "solid-js";
 import { For, Show } from "solid-js";
 import { styled } from "solid-styled-components";
 
-import { ColouredText, Username } from "../design";
+import { ColouredText, Row, Username } from "../design";
 
 /**
  * Base element for the card
@@ -20,6 +20,10 @@ const Base = styled("div", "Tooltip")`
 export function UserCard(
   props: JSX.Directives["floating"]["userCard"] & object
 ) {
+  const roleIds = createMemo(
+    () => new Set(props.member!.orderedRoles.map((role) => role.id))
+  );
+
   return (
     <Base>
       <Show when={props.member}>
@@ -35,7 +39,13 @@ export function UserCard(
         <br />
         <For each={props.member!.orderedRoles}>
           {(role) => (
-            <div>
+            <div
+              onClick={() =>
+                props.member!.edit({
+                  roles: [...roleIds()].filter((id) => id !== role.id),
+                })
+              }
+            >
               <ColouredText
                 colour={role.colour!}
                 clip={role.colour?.includes("gradient")}
@@ -45,6 +55,31 @@ export function UserCard(
             </div>
           )}
         </For>
+        <br />
+        <Row wrap>
+          <For
+            each={props.member?.server?.orderedRoles.filter(
+              (role) => !roleIds().has(role.id)
+            )}
+          >
+            {(role) => (
+              <span
+                onClick={() =>
+                  props.member!.edit({
+                    roles: [...roleIds(), role.id],
+                  })
+                }
+              >
+                <ColouredText
+                  colour={role.colour!}
+                  clip={role.colour?.includes("gradient")}
+                >
+                  {role.name}
+                </ColouredText>
+              </span>
+            )}
+          </For>
+        </Row>
       </Show>
     </Base>
   );
