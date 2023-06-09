1786a7de93a2b51501647a4b2a27b6bc9179541e
brecert <me@bree.dev>
2023-06-09 15:32:47 -0400

    feat: add search to keybinds settings pane

--- components/app/interface/settings/client/Keybinds.tsx
+++ components/app/interface/settings/client/Keybinds.tsx
@@ -4,17 +4,21 @@ import {
   BiSolidXCircle,
 } from "solid-icons/bi";
 import { BiRegularReset } from "solid-icons/bi";
-import { For, Match, Switch } from "solid-js";
+import { For, Match, Switch, createMemo, createSignal } from "solid-js";
 
 import { getController } from "@revolt/common";
 import { useTranslation } from "@revolt/i18n";
 import { KeybindAction } from "@revolt/keybinds";
+import { KeyComboSequence } from "@revolt/keybinds";
+import { KeyCombo } from "@revolt/keybinds";
 import { state } from "@revolt/state";
 import {
   CategoryButton,
   CategoryCollapse,
   Column,
   IconButton,
+  Input,
+  InputElement,
   KeySequence,
   styled,
 } from "@revolt/ui";
@@ -33,6 +37,14 @@ const categories: Record<string, KeybindAction[]> = {
  */
 export default function Keybinds() {
   const t = useTranslation();
+
+  const translateCombo = (combo: KeyCombo, short: boolean) =>
+    combo
+      .map((key) => t(`keys.${key}.${short ? "short" : "full"}`, {}, key))
+      .join("+");
+
+  const translateSequence = (sequence: KeyComboSequence, short: boolean) =>
+    sequence.map((combo) => translateCombo(combo, short)).join(" ");
 
   const editKeybind = (action: KeybindAction, index: number) =>
     getController("modal").push({
@@ -55,85 +67,131 @@ export default function Keybinds() {
   const resetKeybind = (action: KeybindAction, index: number) =>
     state.keybinds.resetKeybindToDefault(action, index);
 
+  const searchData = createMemo(() =>
+    Object.values(categories)
+      .flat()
+      .map((action) => [
+        action,
+        t(`app.settings.pages.keybinds.action.${action}.description`) as string,
+        t(`app.settings.pages.keybinds.action.${action}.title`) as string,
+        ...state.keybinds
+          .getKeybinds()
+          [action].flatMap((sequence) => [
+            translateSequence(sequence, false),
+            translateSequence(sequence, true),
+          ]),
+      ])
+  );
+
+  const [searchText, setSearchText] = createSignal("");
+
+  console.log(searchData());
+  const filteredData = createMemo(() => {
+    console.log(searchText());
+    const foundActions = searchData()
+      .filter((data) =>
+        data.some((text) =>
+          text.toLocaleLowerCase().includes(searchText().toLocaleLowerCase())
+        )
+      )
+      .map((data) => data[0]);
+
+    // fromEntries is generally pretty slow, there may be a better way if search ever feels slow
+    return Object.entries(categories).map(
+      ([k, v]) => [k, v.filter((a) => foundActions.includes(a))] as const
+    );
+  });
+
   // TODO: Tooltips for buttons
   // TODO: a11y pass
   // TODO: separate parts out
   return (
-    <For each={Object.entries(categories)}>
-      {([category, actions]) => (
-        <CategoryCollapse
-          title={t(`app.settings.pages.keybinds.category.${category}`)}
-          open={true}
-        >
-          <Column group={true}>
-            <For each={actions}>
-              {(action) => (
-                <ActionCategory>
-                  <CategoryButton
-                    description={t(
-                      `app.settings.pages.keybinds.action.${action}.description`
-                    )}
-                    action={<BiSolidPlusCircle size={24} />}
-                    onClick={() => addKeybind(action)}
-                  >
-                    {t(`app.settings.pages.keybinds.action.${action}.title`)}
-                  </CategoryButton>
-                  <For each={state.keybinds.getKeybinds()[action]}>
-                    {(sequence, index) => {
-                      const keybindIsDefault = state.keybinds.isDefaultKeybind(
-                        action,
-                        index()
-                      );
+    <Column>
+      <Input
+        type="text"
+        onInput={(e) => setSearchText(e.target.value)}
+        placeholder={t("app.settings.pages.keybinds.search")}
+      />
+      <For each={filteredData()}>
+        {([category, actions]) => (
+          <CategoryCollapse
+            title={t(`app.settings.pages.keybinds.category.${category}`)}
+            open={true}
+          >
+            <Column group="6px">
+              <For each={actions}>
+                {(action) => (
+                  <ActionCategory>
+                    <CategoryButton
+                      description={t(
+                        `app.settings.pages.keybinds.action.${action}.description`
+                      )}
+                      action={<BiSolidPlusCircle size={24} />}
+                      onClick={() => addKeybind(action)}
+                    >
+                      {t(`app.settings.pages.keybinds.action.${action}.title`)}
+                    </CategoryButton>
+                    <For each={state.keybinds.getKeybinds()[action]}>
+                      {(sequence, index) => {
+                        const keybindIsDefault =
+                          state.keybinds.isDefaultKeybind(action, index());
 
-                      const indexIsDefault = state.keybinds.isDefaultIndex(
-                        action,
-                        index()
-                      );
+                        const indexIsDefault = state.keybinds.isDefaultIndex(
+                          action,
+                          index()
+                        );
 
-                      return (
-                        <CategoryButton
-                          action={[
-                            <IconButton
-                              onClick={() => editKeybind(action, index())}
-                            >
-                              <BiSolidPencil size={24}></BiSolidPencil>
-                            </IconButton>,
-                            <Switch>
-                              <Match when={!keybindIsDefault && indexIsDefault}>
-                                <IconButton
-                                  title={t(
-                                    "app.settings.pages.keybinds.remove_keybind"
-                                  )}
-                                  onclick={() => resetKeybind(action, index())}
+                        return (
+                          <CategoryButton
+                            action={[
+                              <IconButton
+                                onClick={() => editKeybind(action, index())}
+                              >
+                                <BiSolidPencil size={24}></BiSolidPencil>
+                              </IconButton>,
+                              <Switch>
+                                <Match
+                                  when={!keybindIsDefault && indexIsDefault}
                                 >
-                                  <BiRegularReset size={24}></BiRegularReset>
-                                </IconButton>
-                              </Match>
-                              <Match when={!keybindIsDefault}>
-                                <IconButton
-                                  title={t(
-                                    "app.settings.pages.keybinds.remove_keybind"
-                                  )}
-                                  onclick={() => resetKeybind(action, index())}
-                                >
-                                  <BiSolidXCircle size={24}></BiSolidXCircle>
-                                </IconButton>
-                              </Match>
-                            </Switch>,
-                          ]}
-                        >
-                          <KeySequence sequence={sequence} short />
-                        </CategoryButton>
-                      );
-                    }}
-                  </For>
-                </ActionCategory>
-              )}
-            </For>
-          </Column>
-        </CategoryCollapse>
-      )}
-    </For>
+                                  <IconButton
+                                    title={t(
+                                      "app.settings.pages.keybinds.remove_keybind"
+                                    )}
+                                    onclick={() =>
+                                      resetKeybind(action, index())
+                                    }
+                                  >
+                                    <BiRegularReset size={24}></BiRegularReset>
+                                  </IconButton>
+                                </Match>
+                                <Match when={!keybindIsDefault}>
+                                  <IconButton
+                                    title={t(
+                                      "app.settings.pages.keybinds.remove_keybind"
+                                    )}
+                                    onclick={() =>
+                                      resetKeybind(action, index())
+                                    }
+                                  >
+                                    <BiSolidXCircle size={24}></BiSolidXCircle>
+                                  </IconButton>
+                                </Match>
+                              </Switch>,
+                            ]}
+                          >
+                            <KeySequence sequence={sequence} short />
+                          </CategoryButton>
+                        );
+                      }}
+                    </For>
+                  </ActionCategory>
+                )}
+              </For>
+            </Column>
+          </CategoryCollapse>
+        )}
+      </For>
+    </Column>
   );
 }
 
