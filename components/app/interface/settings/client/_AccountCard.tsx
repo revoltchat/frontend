5e9d88bcc1b8efedf73096eb6feb5f13e877018a
temptrash <135544763+trashtemp@users.noreply.github.com>
2023-06-19 10:11:22 +0200

    fix: finished text overflow for accountcard

--- components/app/interface/settings/client/_AccountCard.tsx
+++ components/app/interface/settings/client/_AccountCard.tsx
@@ -1,6 +1,6 @@
 import { useClient } from "@revolt/client";
 import { useTranslation } from "@revolt/i18n";
-import { Avatar, Column, Row, Typography, useTheme, iconSize } from "@revolt/ui";
+import { Avatar, Column, Row, Typography, styled, useTheme, iconSize } from "@revolt/ui";
 import { useSettingsNavigation } from "../Settings";
 import { SidebarButton } from "../_layout/SidebarButton";
 
@@ -16,19 +16,58 @@ export function AccountCard() {
   const theme = useTheme();
 
   return (
-    <SidebarButton onClick={() => navigate("account")}>
-      <Row align gap="md">
+    <SidebarAccountCard onClick={() => navigate("account")}>
+      <div class="title">
         <Avatar src={client().user!.animatedAvatarURL} size={36} />
-        <Column gap="xs">
+        <div class="text">
           <Typography variant="settings-account-card-title">
             {client().user!.username}
           </Typography>
           <Typography variant="settings-account-card-subtitle">
             {t("app.settings.pages.account.title")}
           </Typography>
-        </Column>
+        </div>
+      </div>
+      <div class="icon">
         <MdError {...iconSize(20)} fill={theme!.colour("primary")} />
-      </Row>
-    </SidebarButton>
+        <MdError {...iconSize(20)} fill={theme!.colour("primary")} />
+      </div>
+    </SidebarAccountCard>
   );
 }
+
+const SidebarAccountCard = styled(SidebarButton)`
+  .title {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    flex-grow: 1;
+    padding-inline-end: 8px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    
+    .text {
+      display: flex;
+      flex-direction: column;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      span {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+  }
+
+  .icon {
+    display: flex;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    flex-shrink: 0;
+    gap: 2px;
+  }
+`;
