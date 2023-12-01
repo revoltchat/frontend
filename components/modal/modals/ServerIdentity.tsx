4038c02110239e0b73b50fdb960de4f22f03e738
Paul Makles <me@insrt.uk>
2023-12-01 12:31:04 +0000

    chore: wip server identity

--- components/modal/modals/ServerIdentity.tsx
+++ components/modal/modals/ServerIdentity.tsx
@@ -1,9 +1,35 @@
-import { createSignal } from "solid-js";
+import { Accessor, createEffect, createSignal } from "solid-js";
+
+import { ServerMember } from "revolt.js";
 
 import { useTranslation } from "@revolt/i18n";
-import { Column, Input } from "@revolt/ui";
+import { Avatar, Column, Input, MessageContainer, Username } from "@revolt/ui";
 
 import { PropGenerator } from "../types";
+
+function Preview(props: { nickname: Accessor<string>; member: ServerMember }) {
+  createEffect(() => {
+    console.info("n:", props.nickname());
+  });
+
+  return (
+    <>
+      <span>Preview</span>
+      <MessageContainer
+        avatar={<Avatar size={36} src={props.member.animatedAvatarURL} />}
+        timestamp={new Date()}
+        username={
+          <Username
+            username={props.nickname()}
+            colour={props.member.roleColour!}
+          />
+        }
+      >
+        Hello {props.nickname()}!
+      </MessageContainer>
+    </>
+  );
+}
 
 /**
  * Modal to update the user's server identity
@@ -11,6 +37,8 @@ import { PropGenerator } from "../types";
 const ServerIdentity: PropGenerator<"server_identity"> = (props) => {
   const t = useTranslation();
   const [nickname, setNickname] = createSignal(props.member.nickname ?? "");
+
+  const [avatarFile, setAvatarFile] = createSignal<File>();
 
   return {
     title: t("app.special.popovers.server_identity.title", {
@@ -25,6 +53,8 @@ const ServerIdentity: PropGenerator<"server_identity"> = (props) => {
           onChange={(e) => setNickname(e.currentTarget.value)}
         />
         <span>{t("app.special.popovers.server_identity.avatar")}</span>
+        <Avatar size={64} src={props.member.animatedAvatarURL} interactive />
+        {/* <Preview nickname={nickname} member={props.member} /> */}
       </Column>
     ),
     actions: [
