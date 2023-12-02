b458f7fa9bb4f87ff60e902f671cb977b36bdcdd
Paul Makles <me@insrt.uk>
2023-12-02 10:56:53 +0000

    feat: configure Sentry error reporting

--- packages/client/src/sentry.ts
+++ packages/client/src/sentry.ts
@@ -1,10 +1,19 @@
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
 
-if (import.meta.env.PROD) {
-  /* Sentry.init({
-    dsn: // TODO: migrate to Revolt Sentry
-    integrations: [new BrowserTracing()],
+if (import.meta.env.PROD && import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      new BrowserTracing({
+        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", /^https:\/\/revolt\.chat\/api/],
+      }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
-  }); */
+  });
 }
