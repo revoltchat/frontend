f65d31d0c9e270b1b691e86e785077704aaf1d9f
Paul Makles <me@insrt.uk>
2023-12-07 12:29:03 +0000

    feat: show time since last message feat: dismiss new messages indicator

--- components/ui/components/messaging/indicators/NewMessages.tsx
+++ components/ui/components/messaging/indicators/NewMessages.tsx
@@ -1,7 +1,12 @@
 import { Accessor, Show } from "solid-js";
 
+import { decodeTime } from "ulid";
+
 import { dayjs, useTranslation } from "@revolt/i18n";
 
+import MdClose from "@material-design-icons/svg/filled/close.svg?component-solid";
+
+import { iconSize, styled } from "../../..";
 import { ripple } from "../../../directives";
 
 import { FloatingIndicator } from "./FloatingIndicator";
@@ -17,12 +22,12 @@ interface Props {
   /**
    * Jump back to the last message
    */
-  // jumpBack: () => void;
+  jumpBack: () => void;
 
   /**
    * Dismiss the message
    */
-  // dismiss: () => void;
+  dismiss: () => void;
 }
 
 /**
@@ -31,13 +36,33 @@ interface Props {
 export function NewMessages(props: Props) {
   const t = useTranslation();
 
+  // TODO: hook escape button
+
+  /**
+   * Remove the message
+   */
+  function onCancel(e: MouseEvent) {
+    e.stopPropagation();
+    props.dismiss();
+  }
+
   return (
-    <Show when={props.lastId() || true}>
-      <FloatingIndicator use:ripple position="top">
-        {t("app.main.channel.misc.new_messages", {
-          time_ago: dayjs(/*decodeTime(props.lastId())*/).fromNow(),
-        })}
+    <Show when={props.lastId()}>
+      <FloatingIndicator use:ripple position="top" onClick={props.jumpBack}>
+        <span style={{ "flex-grow": 1 }}>
+          {t("app.main.channel.misc.new_messages", {
+            time_ago: dayjs(decodeTime(props.lastId()!)).fromNow(),
+          })}
+        </span>
+        <span>{t("app.main.channel.misc.jump_beginning")}</span>
+        <CancelIcon onClick={onCancel}>
+          <MdClose {...iconSize(16)} />
+        </CancelIcon>
       </FloatingIndicator>
     </Show>
   );
 }
+
+const CancelIcon = styled.div`
+  height: 16px;
+`;
