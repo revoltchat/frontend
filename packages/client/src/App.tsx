c67b453399965e3aafd47987a9bf3a9f93f8359d
Paul Makles <me@insrt.uk>
2024-03-17 00:03:14 +0000

    chore: put correct routing back in

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,47 +1,19 @@
-import { Component, Show, Suspense, Switch, lazy } from "solid-js";
+import { Component, lazy } from "solid-js";
 
 import { Route, Routes } from "@revolt/routing";
-// import { Button } from "@revolt/ui";
-import { Button } from "@revolt/ui/components/design/atoms/inputs/Button";
 
-// const AuthPage = lazy(() => import("./Auth"));
-// const Interface = lazy(() => import("./Interface"));
-const Chunked = lazy(() => import("./Chunked"));
-
-/*async function Lazy(props: { cb: () => Promise<Component> }) {
-  let C: Component;
-
-  return <Show when={false}>
-    <C />
-  </Show>
-}*/
-
-const Test = lazy(async () => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  return { default: () => <h1>hello!</h1> };
-});
+const AuthPage = lazy(() => import("./Auth"));
+const Interface = lazy(() => import("./Interface"));
 
 /**
  * App routing
  */
 const App: Component = () => {
   return (
-    <>
-      <Button>panda css does not break in production :eyes:</Button>
-      <Suspense>
-        <Test />
-      </Suspense>
-      <Suspense>
-        <Chunked />
-      </Suspense>
-      {/*<Suspense fallback={<h1>waiting for load...</h1>}>
-        <Chunked />
-  </Suspense>*/}
-    </>
-    // <Routes>
-    //   <Route path="/login/*" component={AuthPage} />
-    //   <Route path="/*" component={Interface} />
-    // </Routes>
+    <Routes>
+      <Route path="/login/*" component={AuthPage} />
+      <Route path="/*" component={Interface} />
+    </Routes>
   );
 };
 
--- packages/client/src/Chunked.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@revolt/ui/components/design/atoms/inputs/Button";
-
-export default function T() {
-  return (
-    <Button variant="secondary">
-      look, im a lazy component! and i did not shitted like styled components!
-    </Button>
-  );
-}
