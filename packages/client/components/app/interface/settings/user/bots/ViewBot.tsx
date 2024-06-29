188ef8477ec47e65f03ab4286397e20510adb08f
Paul Makles <me@insrt.uk>
2024-06-29 19:06:08 +0100

    feat: scaffold bot buttons

--- packages/client/components/app/interface/settings/user/bots/ViewBot.tsx
+++ packages/client/components/app/interface/settings/user/bots/ViewBot.tsx
@@ -1,6 +1,16 @@
 import { useClient } from "@revolt/client";
 import { createProfileResource } from "@revolt/client/resources";
-import { Column } from "@revolt/ui";
+import {
+  CategoryButton,
+  CategoryButtonGroup,
+  Column,
+  iconSize,
+} from "@revolt/ui";
+
+import MdLink from "@material-design-icons/svg/outlined/link.svg?component-solid";
+import MdPersonAdd from "@material-design-icons/svg/outlined/person_add.svg?component-solid";
+import MdPublic from "@material-design-icons/svg/outlined/public.svg?component-solid";
+import MdToken from "@material-design-icons/svg/outlined/token.svg?component-solid";
 
 import { useSettingsNavigation } from "../../Settings";
 import { UserSummary } from "../account";
@@ -27,6 +37,35 @@ export function ViewBot() {
       {/* <ErrorBoundary fallback={<>Failed to load profile</>}>
         <Suspense fallback={<>loading...</>}>{profile.data?.content}</Suspense>
       </ErrorBoundary> */}
+
+      <CategoryButtonGroup>
+        <CategoryButton
+          description="Generate a new token if it gets lost or compromised"
+          icon={<MdToken {...iconSize(22)} />}
+          action="chevron"
+        >
+          Reset Token
+        </CategoryButton>
+        <CategoryButton
+          description="Allow others to add your bot to their servers from Discover"
+          icon={<MdPublic {...iconSize(22)} />}
+          action="chevron"
+        >
+          Submit to Discover
+        </CategoryButton>
+      </CategoryButtonGroup>
+
+      <CategoryButtonGroup>
+        <CategoryButton icon={<MdLink {...iconSize(22)} />} action="copy">
+          Copy Invite
+        </CategoryButton>
+        <CategoryButton
+          icon={<MdPersonAdd {...iconSize(22)} />}
+          action="chevron"
+        >
+          Invite Bot
+        </CategoryButton>
+      </CategoryButtonGroup>
     </Column>
   );
 }
