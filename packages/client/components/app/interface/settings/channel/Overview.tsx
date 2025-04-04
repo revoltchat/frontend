54d45927376a72c078924ce32366c748f6e1dab2
izzy <me@insrt.uk>
2025-04-04 16:53:18 +0100

    feat: ability to mark channel as mature

--- packages/client/components/app/interface/settings/channel/Overview.tsx
+++ packages/client/components/app/interface/settings/channel/Overview.tsx
@@ -1,12 +1,13 @@
 import { createFormControl, createFormGroup } from "solid-forms";
-import { Show } from "solid-js";
+import { Match, Show, Switch } from "solid-js";
 
 import { Trans, useLingui } from "@lingui-solid/solid/macro";
+import { useMutation } from "@tanstack/solid-query";
 import type { API } from "revolt.js";
 
 import { useClient } from "@revolt/client";
 import { CONFIGURATION } from "@revolt/common";
-import { CircularProgress, Column, Form2, Row, Text } from "@revolt/ui";
+import { Button, CircularProgress, Column, Form2, Row, Text } from "@revolt/ui";
 
 import { ChannelSettingsProps } from "../ChannelSettings";
 
@@ -17,6 +18,8 @@ export default function ChannelOverview(props: ChannelSettingsProps) {
   const { t } = useLingui();
   const client = useClient();
 
+  /* eslint-disable solid/reactivity */
+  // we want to take the initial value only
   const editGroup = createFormGroup({
     name: createFormControl(props.channel.name),
     description: createFormControl(props.channel.description || ""),
@@ -24,6 +27,7 @@ export default function ChannelOverview(props: ChannelSettingsProps) {
       props.channel.animatedIconURL,
     ),
   });
+  /* eslint-enable solid/reactivity */
 
   function onReset() {
     editGroup.controls.name.setValue(props.channel.name);
@@ -76,6 +80,13 @@ export default function ChannelOverview(props: ChannelSettingsProps) {
     await props.channel.edit(changes);
   }
 
+  /**
+   * Mutation handler for changing channel mature status
+   */
+  const changeMature = useMutation(() => ({
+    mutationFn: (nsfw: boolean) => props.channel.edit({ nsfw }),
+  }));
+
   return (
     <Column gap="xl">
       <form onSubmit={Form2.submitHandler(editGroup, onSubmit, onReset)}>
@@ -108,6 +119,29 @@ export default function ChannelOverview(props: ChannelSettingsProps) {
           </Row>
         </Column>
       </form>
+      <Column>
+        <Text class="label">
+          <Trans>Mark as Mature</Trans>
+        </Text>
+        <Text>
+          <Trans>
+            Users will be asked to confirm their age before opening this
+            channel.
+          </Trans>
+        </Text>
+        <div>
+          <Button // todo: confirmation modal
+            isDisabled={changeMature.isPending}
+            onPress={() => changeMature.mutate(!props.channel.mature)}
+          >
+            <Switch fallback={<Trans>Mark as Mature</Trans>}>
+              <Match when={props.channel.mature}>
+                <Trans>Unmark as Mature</Trans>
+              </Match>
+            </Switch>
+          </Button>
+        </div>
+      </Column>
     </Column>
   );
 }
