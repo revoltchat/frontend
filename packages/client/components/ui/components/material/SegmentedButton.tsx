a3d11900a43e096c865e9fb7152b780af2b6654e
izzy <me@insrt.uk>
2025-05-31 12:17:00 +0100

    feat: segmented button design

--- packages/client/components/ui/components/material/SegmentedButton.tsx
+++ packages/client/components/ui/components/material/SegmentedButton.tsx
@@ -1,12 +1,42 @@
 import "mdui/components/segmented-button-group.js";
 import "mdui/components/segmented-button.js";
+import { cva } from "styled-system/css";
 
 export function SingleSelectSegmentedButton() {
   return (
-    <mdui-segmented-button-group selects="single" onChange={() => {}}>
-      <mdui-segmented-button value="day">Allow</mdui-segmented-button>
-      <mdui-segmented-button value="week">Inherit</mdui-segmented-button>
-      <mdui-segmented-button value="month">Disallow</mdui-segmented-button>
+    <mdui-segmented-button-group
+      selects="single"
+      value="allow"
+      onChange={() => {}}
+    >
+      <mdui-segmented-button class={styles()} value="allow">
+        Allow
+      </mdui-segmented-button>
+      <mdui-segmented-button class={styles()} value="inherit">
+        Inherit
+      </mdui-segmented-button>
+      <mdui-segmented-button class={styles()} value="disallow">
+        Disallow
+      </mdui-segmented-button>
     </mdui-segmented-button-group>
   );
 }
+
+const styles = cva({
+  base: {
+    border: "1px solid var(--md-sys-color-on-surface)",
+
+    "&:nth-child(1)": {
+      borderLeft: "1px solid var(--md-sys-color-on-surface)",
+    },
+
+    "&:not(:nth-child(1))": {
+      borderLeft: "none",
+    },
+
+    "&[selected]": {
+      color: "rgb(var(--mdui-color-on-secondary-container))",
+      background: "rgb(var(--mdui-color-secondary-container))",
+    },
+  },
+});
