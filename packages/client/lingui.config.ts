4f49fa3b1c252cee4f204d4c8e8730e2c895c89c
izzy <me@insrt.uk>
2025-08-25 19:25:33 +0200

    fix: regenerate message catalogs

--- packages/client/lingui.config.ts
+++ packages/client/lingui.config.ts
@@ -2,6 +2,12 @@ import extractor from "@lingui-solid/babel-plugin-extract-messages/extractor";
 import { defineConfig } from "@lingui/cli";
 import { LinguiConfig } from "@lingui/conf";
 
+/* eslint-disable */
+const supressWarningIfWereNotInLinguiExtract = !
+// @ts-expect-error
+(process as any)?.argv[1]?.includes("lingui-extract.js");
+/* eslint-enable */
+
 export default defineConfig({
   sourceLocale: "en",
   locales: ["en", "dev"],
@@ -12,13 +18,17 @@ export default defineConfig({
       exclude: ["**/node_modules/**", "**/i18n/locales/**"],
     },
   ],
-  // macro: {
-  //   corePackage: ["@lingui-solid/solid"],
-  //   jsxPackage: ["@lingui-solid/solid/macro"],
-  // },
   runtimeConfigModule: {
     Trans: ["@lingui-solid/solid", "Trans"],
     useLingui: ["@lingui-solid/solid", "useLingui"],
     extractors: [extractor],
   },
+  ...(supressWarningIfWereNotInLinguiExtract
+    ? {}
+    : {
+        macro: {
+          corePackage: ["@lingui-solid/solid"],
+          jsxPackage: ["@lingui-solid/solid/macro"],
+        },
+      }),
 } as LinguiConfig);
