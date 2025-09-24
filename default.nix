c28975b9765d2a7f4cf938f6380e9630b75290c3
izzy <me@insrt.uk>
2025-09-24 15:13:42 -0500

    chore: update lockfile

--- default.nix
+++ default.nix
@@ -1,27 +1,23 @@
-let
-  # Pinned nixpkgs, deterministic. Last updated: 29-10-2024.
-  pkgs = import (fetchTarball (
-    "https://github.com/NixOS/nixpkgs/archive/0930fa9bab0e3877d4ab0682f805b2dc86ec5716.tar.gz"
-  )) { };
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
-  # Rolling updates, not deterministic.
-  # pkgs = import (fetchTarball ("channel:nixpkgs-unstable")) { };
-in
+with pkgs;
 pkgs.mkShell {
   name = "revoltEnv";
 
   buildInputs = [
     # Tools
-    pkgs.git
-    pkgs.gh
-    pkgs.deno
+    git
+    gh
+    deno
 
     # Node
-    pkgs.nodejs
-    pkgs.nodejs.pkgs.pnpm
+    nodejs
+    nodejs.pkgs.pnpm
 
     # mdbook
-    pkgs.mdbook
-    pkgs.mdbook-mermaid
+    mdbook
+    mdbook-mermaid
   ];
 }
